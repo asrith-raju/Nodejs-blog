bfa8cee64381c59a6b8433165e9dac8e465fc474
asrith-raju <asrithraju143@gmail.com>
2025-09-12 12:13:03 +0530

    feat: created user registration

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')
+const session = require('express-session')
+
 const connectDB = require('./server/config/db.js')
 const app = express();
 
@@ -12,7 +16,17 @@ connectDB();
 
 app.use(express.urlencoded({ extended : true }));
 app.use(express.json());
-
+app.use(cookieParser())
+app.use(session({
+    secret : 'keyboard cat',
+    resave:false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URI,
+        // ttl: 14 * 24 * 60 * 60  
+    })
+    
+}))
 app.use(express.static('public'));
 
 //Templating Engine
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -2,10 +2,11 @@ const express = require('express')
 const router = express.Router()
 const Post = require('../models/post.js');
 const User = require('../models/User.js');
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const adminLayout = '../views/layouts/admin'
-
+jwtSecret= process.env.JWT_SECRET;
 // GET 
 // Admin-LOGIN PAGE 
 
@@ -22,8 +23,6 @@ router.get('/admin',async (req,res)=>{
         console.log(error);
         
     }
-
-    
 });
 
 // POSt
@@ -33,13 +32,58 @@ router.get('/admin',async (req,res)=>{
 router.post('/admin',async (req,res)=>{
     try {
     const {username,password} = req.body;
-    console.log(req.body);
     
+    const user = await User.findOne({username});
      
-         await Post.deleteMany();
-        const data = await Post.find();
-        // res.render('admin/index',{locals,layout : adminLayout});
-        res.redirect('/admin')
+    if(!user){
+        return res.status(401).json({message: 'invalid credentials'})
+    }
+     const isPasswordValid = await bcrypt.compare(password,user.password)
+     if(!isPasswordValid){
+        return res.status(401).json({message: 'invalid credentials'})
+    }
+
+    const  token = jwt.sign({userId: user._id},jwtSecret);
+    res.cookie('token',token,{httpOnly: true});
+
+    res.redirect('/dashboard')
+
+
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+});
+
+// POSt
+// Admin- Check LOGIN PAGE 
+
+
+router.get('/dashboard',async (req,res)=>{
+
+    res.render('admin/dashboard');
+
+});
+// POST
+// Admin - Register
+
+router.post('/register',async (req,res)=>{
+    try {
+    const {username,password} = req.body;
+    
+   const hashedPassword = await bcrypt.hash(password,10);
+   
+   try {
+    const user = await User.create({username,password:hashedPassword});
+    res.status(201).json({message:'user created',user})
+   } catch (error) {
+    if(error.code === 11000){
+        res.status(409).json({message:'user already in use'})
+    }
+    res.status(500).json({message: 'internal server error'})
+   }
+
     } catch (error) {
         console.log(error);
         
