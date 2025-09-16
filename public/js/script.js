01bd3cde49002ef1952a5d315881bafd4d5f2f20
asrith-raju <asrithraju143@gmail.com>
2025-09-16 18:41:14 +0530

    feat: added dark theme

--- public/js/script.js
+++ public/js/script.js
@@ -1,33 +1,39 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
     const allButtons = document.querySelectorAll('.searchBtn')
     const searchBar = document.querySelector('.searchBar')
     const searchInput = document.getElementById('searchInput')
     const searchClose = document.getElementById('searchClose')
 
-    for(var i = 0; i < allButtons.length;i++){
-        allButtons[i].addEventListener('click',function(){
+    for (var i = 0; i < allButtons.length; i++) {
+        allButtons[i].addEventListener('click', function () {
             searchBar.style.visibility = 'visible';
             searchBar.classList.add('open');
-            this.setAttribute('aria-expanded','true');
+            this.setAttribute('aria-expanded', 'true');
             searchInput.focus();
         })
-        searchClose.addEventListener('click',function(){
+        searchClose.addEventListener('click', function () {
             searchBar.style.visibility = 'hidden';
             searchBar.classList.remove('open');
-            this.setAttribute('aria-expanded','false');     
+            this.setAttribute('aria-expanded', 'false');
         })
     }
 })
 const darktheme = document.getElementById('dark')
 const light = document.getElementById('light')
-const temp=true
-darktheme.addEventListener('click',()=>{
-    if(temp){
-        document.body.style.background = "black"
-        document.body.style.color="white"
-        document.light.classList.remove('hide')
-        document.darktheme.classList.add('hide')
-        temp=false
-    }
+const r = document.querySelector(':root');
 
+let temp = true
+darktheme.addEventListener('click', () => {
+     light.classList.remove('hide');
+  darktheme.classList.add('hide');
+    r.style.setProperty('--bg-color', '#1c1c1c');
+    r.style.setProperty('--text-color', '#faf5ee');
+    document.body.style.backgroundImage = "none";
+})
+light.addEventListener('click', () => {
+    r.style.setProperty('--bg-color', '#faf5ee');   
+  r.style.setProperty('--text-color', '#1c1c1c'); 
+  light.classList.add('hide');
+  darktheme.classList.remove('hide');
+    document.body.style.backgroundImage = "url('/images/img-noise.png')";
 })
